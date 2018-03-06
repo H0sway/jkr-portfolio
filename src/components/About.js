2d291cd72aacd26e77be70d759f4570de759a396
Joshua Russell <jkrussell756@gmail.com>
2018-03-06 10:27:47 -0500

    structure for about component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {  } from 'react-bootstrap';
+
+const About = () => {
+  return (
+    <div className="About">
+    </div>
+  )
+}
+
+export default About;
