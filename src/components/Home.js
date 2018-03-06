34ea1ffa1136ae1e9260cf4ad6062e82d26df95d
Joshua Russell <jkrussell756@gmail.com>
2018-03-06 10:21:42 -0500

    fixed centering issue, bootstrap is weird

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,16 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <Row>
-          <Col xs={11} md={9}>
-            <div className="welcome">
-              <Jumbotron>
-                <Image src={face} className="face-photo" thumbnail responsive />
-                <h3>Joshua K. Russell</h3>
-                <p>NYC based full-stack web developer. Click the button below to continue to my site.</p>
-                <LinkContainer to="/about">
-                  <Button bsStyle="info">About Me</Button>
-                </LinkContainer>
-              </Jumbotron>
-            </div>
-          </Col>
-        </Row>
+        <div className="welcome">
+          <Jumbotron>
+            <Image src={face} className="face-photo" thumbnail responsive />
+            <h3>Joshua K. Russell</h3>
+            <p>NYC based full-stack web developer. Click the button below to continue to my site.</p>
+            <LinkContainer to="/about">
+              <Button bsStyle="info">About Me</Button>
+            </LinkContainer>
+          </Jumbotron>
+        </div>
       </div>
     )
   }
