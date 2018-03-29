fbb66a08aa349b830cc81c19057982d3e5a45cba
Joshua Russell <jkrussell756@gmail.com>
2018-03-29 16:25:47 -0400

    changed a project url

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,7 +28,7 @@ class Projects extends Component {
             <Project
               title="Puppy Finder"
               short="Search for puppies up for adoption."
-              url="https://puppyfinderapp.herokuapp.com/"
+              url="https://fathomless-bayou-33817.herokuapp.com/"
               github="https://github.com/H0sway/puppy-finder-app"
               img={puppy}
               long="Search for puppies up for adoption and save your favorites to a list. This application allows you to search the Pet Finder API for specific dog breeds up for adoption. Just select a breed in the dropdown menu, type in your zipcode, and hit enter! You can save puppies you're interested in by clicking the save button. Navigate to the favorites page and click on a puppy to add personal notes for each puppy along with a rating. Remember, all dogs are good dogs. I collaborated with three others while developing the site. This app was developed using React, Bootstrap, Node, Express, Webpack, and Postgres."
