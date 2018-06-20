fe1d99b5ddb647d396f713378792ad064366c83e
Joshua Russell <jkrussell756@gmail.com>
2018-06-20 10:19:15 -0400

    added the skills component, started on the basic structure

---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Panel } from 'react-bootstrap';
+
+const list = [
+  {
+    title: "HTML",
+    body: "Bottom Text"
+  },
+  {
+    title: "CSS",
+    body: "Bottom Text"
+  },
+  {
+    title: "JavaScript",
+    body: "Bottom Text"
+  },
+  {
+    title: "Ruby",
+    body: "Bottom Text"
+  },
+]
+
+const addPanels = () => {
+  for (i = 0; i < list.length; i++) {
+    return()
+  }
+}
+
+const Skills = () => {
+  return (
+    {addPanels()}
+  )
+}
+
+export default Skills;
