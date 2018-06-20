c94f42eab93719db0ca42e612de0f3801fbd0dab
Joshua Russell <jkrussell756@gmail.com>
2018-06-20 12:24:28 -0400

    made some changes to bootstrap columns

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,11 @@ const About = () => {
         <p className="about-text">I was born in New York City and lived here for most of my life. Full-stack web developer with experience working in education and for non-profit organizations. Graduate of General Assembly's Web Development Immersive. I'm passionate about a great deal of things, but what drives me right now is my desire to improve on my abilities as well as finding new and creative ways to apply what I already know. You can get in touch with me through my contact information listed below, or you can click on the projects link up above to see some of the things I've worked on.</p>
       </Jumbotron>
       <Row className="about-wrapper">
-        <Col xs={12} md={4}>
+        <Col xs={8} md={4}>
           <h3>My Skills</h3>
           <Skills />
         </Col>
-        <Col xs={12} md={4}>
+        <Col xs={8} md={4}>
           <Contact />
         </Col>
       </Row>
