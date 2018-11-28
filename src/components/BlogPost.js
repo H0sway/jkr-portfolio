152c3048b44f8583662ddc80ef07fa2b5aa02143
Joshua Russell <jkrussell756@gmail.com>
2018-11-28 12:19:48 -0500

    Added the medium logo to the individual blog posts, first draft of styling for the entire Blog route completed.

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
+
+import mediumLogo from './images/Monogram.png';
 
 const BlogPost = (props) => {
   return (
     <div className="BlogPost">
       <h4 className="blog-title">{props.title}</h4>
       <p className="blog-subtitle">{props.subtitle}</p>
-      <Button href={props.url} target="_blank" rel="noopener noreferrer">Read More!</Button>
+      <br />
+
+
+      <Button href={props.url} target="_blank" rel="noopener noreferrer">
+        <Image className='medium-logo' src={mediumLogo} responsive />
+        Read More!
+      </Button>
     </div>
   )
 }
