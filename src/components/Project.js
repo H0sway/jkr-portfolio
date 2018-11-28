2572c84b0350a1429ca0d4f3843de113298faaff
Joshua Russell <jkrussell756@gmail.com>
2018-11-28 11:20:20 -0500

    Fixed a minor semantics error

--- src/components/Project.js
+++ src/components/Project.js
@@ -19,27 +19,27 @@ class Project extends Component {
   render() {
     return (
       <div className="single-project">
-          <h2 className="title">{this.props.title}</h2>
-          <Image className="project-img" src={this.props.img} alt={this.props.title} thumbnail responsive />
-          <p className="short">{this.props.short}</p>
-          <button className="learn-more" onClick={this.handleShow}>Learn More</button>
-          <Modal show={this.state.show} onHide={this.handleClose}>
-            <Modal.Header closebutton>
-              <Modal.Title className="modal-title">{this.props.title}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Image className="modal-img" src={this.props.img} rounded responsive />
-              <p className="long">{this.props.long}</p>
-              <ButtonGroup>
-                <Button href={this.props.url} target="_blank" rel="noopener noreferrer">To the site!</Button>
-                <Button href={this.props.github} target="_blank" rel="noopener noreferrer">Github</Button>
-              </ButtonGroup>
+        <h2 className="title">{this.props.title}</h2>
+        <Image className="project-img" src={this.props.img} alt={this.props.title} thumbnail responsive />
+        <p className="short">{this.props.short}</p>
+        <button className="learn-more" onClick={this.handleShow}>Learn More</button>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closebutton>
+            <Modal.Title className="modal-title">{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Image className="modal-img" src={this.props.img} rounded responsive />
+            <p className="long">{this.props.long}</p>
+            <ButtonGroup>
+              <Button href={this.props.url} target="_blank" rel="noopener noreferrer">To the site!</Button>
+              <Button href={this.props.github} target="_blank" rel="noopener noreferrer">Github</Button>
+            </ButtonGroup>
 
-              <br />
+            <br />
 
-              <button className="close-modal" onClick={this.handleClose}>Close</button>
-            </Modal.Body>
-          </Modal>
+            <button className="close-modal" onClick={this.handleClose}>Close</button>
+          </Modal.Body>
+        </Modal>
       </div>
     )
   }
