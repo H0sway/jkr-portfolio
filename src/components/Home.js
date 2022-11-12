193c5c683d6ad81e7e700aa94d473a463fbe6757
Joshua K. Russell <33361925+H0sway@users.noreply.github.com>
2022-11-11 20:17:04 -0500

    Update Home.js
    I moved

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
           <Jumbotron>
             <Image src={face} className="face-photo" thumbnail responsive />
             <h3 className="name">Joshua K. Russell</h3>
-            <h5 className="clickbait">NYC based full-stack web developer. Click the button below to continue to my site.</h5>
+            <h5 className="clickbait">Kingston based full-stack web developer. Click the button below to continue to my site.</h5>
             <LinkContainer to="/about">
               <button className="about-button">About Me</button>
             </LinkContainer>
